b6f37fc001b4a8c00251f59a9ee35ef2a7ea44b2
Arnaud04 <lenaud2004@gmail.com>
2026-08-04 22:37:25 +0300

    Add completed field to Todo

--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) { }
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+
+    @ApiPropertyOptional({
+        example: true,
+        description: 'Todo terminé ou non',
+    })
+    @IsOptional()
+    @IsBoolean()
+    completed?: boolean;
+
+}
